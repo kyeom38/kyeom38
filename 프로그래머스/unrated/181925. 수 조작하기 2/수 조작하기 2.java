3dc7ab038aa408a9e6560247afe78b426e6678d6
kyeom38 <113573228+kyeom38@users.noreply.github.com>
2023-06-06 18:50:18 +0900

    [unrated] Title: 수 조작하기 2, Time: 623.19 ms, Memory: 373 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public String solution(int[] numLog) 
+    {
+        String answer = "";
+       
+        for (int i =1;i <numLog.length;i++)
+        {
+            int cal = numLog[i-1] - numLog[i];
+            if (numLog[i-1] < numLog[i] && cal ==-1)
+            {
+                answer += "w";
+            }
+            else if (numLog[i-1] > numLog[i] && cal == 1)
+            {
+                answer += "s";
+            }
+            else if (numLog[i-1] < numLog[i] && cal == -10)
+            {
+                answer += "d";
+            }
+            else if (numLog[i-1] > numLog[i] && cal == 10)
+            {
+                answer += "a";
+            }
+        }
+        return answer;
+    }
+}

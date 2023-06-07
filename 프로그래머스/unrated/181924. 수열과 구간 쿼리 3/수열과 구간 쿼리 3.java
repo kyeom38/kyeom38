ee36f599444b77ea5f8eb4ac8c516434e0a128e3
kyeom38 <113573228+kyeom38@users.noreply.github.com>
2023-06-07 14:05:17 +0900

    [unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 86.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,15 @@
+class Solution 
+{
+    public int[] solution(int[] arr, int[][] queries) 
+    {
+        for ( int[] query : queries)
+        {
+            int i = query[0];
+            int j = query[1];
+            int empty = arr[i];
+            arr [i] = arr[j];
+            arr[j] = empty;
+        }
+        return arr;
+    }
+}

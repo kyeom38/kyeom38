4deaabeee95ed10558cf4c54d2a8391a47149447
kyeom38 <113573228+kyeom38@users.noreply.github.com>
2023-06-13 13:46:32 +0900

    [unrated] Title: 문자열 바꿔서 찾기, Time: 1.75 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,24 @@
+class Solution 
+{
+    public int solution(String myString, String pat) 
+    {
+        int answer = 0;
+        String change ="";
+        for(int i =0; i<myString.length();i++)
+        {
+            if(myString.charAt(i)=='A')
+            {
+                change += "B";
+            }
+            else if (myString.charAt(i) =='B')
+            {
+                change += "A";
+            }
+        }
+        if (change.contains(pat))
+        {
+            answer = 1;
+        }
+        return answer;
+    }
+}
